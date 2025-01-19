4fc8f89693527da1803848c36976215e7f938b87
sekharjpapp <43300292+sekharjpapp@users.noreply.github.com>
2025-01-19 18:18:32 +0530

    changes in java main

--- src/main/java/com/pixel/javafxdemo/HelloApplication.java
+++ src/main/java/com/pixel/javafxdemo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.pixel.javafxdemo;
 import com.pixel.javafxdemo.util.HibernateUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.hibernate.Session;
@@ -11,11 +12,23 @@ import org.hibernate.SessionFactory;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Scene scene;
+
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainView.fxml"))));
-        primaryStage.setTitle("Product Management");
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("employee"));
+        stage.setTitle("JavaFx Emp Hibernate App");
+        stage.setScene(scene);
+        stage.show();
+    }
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
@@ -23,7 +36,7 @@ public class HelloApplication extends Application {
         Session session = sessionFactory.openSession();
         String version = (String) session.createNativeQuery("select version()").getSingleResult();
         System.out.println("MYSQL Version : " + version);
-       // launch();
+       launch();
 
     }
 }
